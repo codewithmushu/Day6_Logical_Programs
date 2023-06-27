c5606442a0f0cba374bd5d05ded80461134d9295
Muskan Shaikh <shaikhitsmuskan@gmail.com>
2023-06-27 21:53:35 +0530

    [Muskan] Add : Fibonacci Series Fibonacci series is a special type of series in which the next term is the sum of the previous two terms. For example, if 0 and 1 are the two previous terms in a series, then the next term will be 1(0+1).

---
+++ Day6LogicalPrograms/LogicalPrograms.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6LogicalPrograms
+{
+    public class LogicalPrograms
+    {
+        public static void FibonacciSeries()
+        {
+            Console.WriteLine("Enter the number of terms:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int firstTerm = 0;
+            int secondTerm = 1;
+
+            Console.WriteLine("Fibonacci Series:");
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(firstTerm + " ");
+
+                int nextTerm = firstTerm + secondTerm;
+                firstTerm = secondTerm;
+                secondTerm = nextTerm;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Day6LogicalPrograms/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6LogicalPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1. Fibonacci Series");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("Write your choice here : ");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        LogicalPrograms.FibonacciSeries();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exiting...");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+                Console.WriteLine();
+            } while (choice != 0);
+        }
+        
+    }
+}
